29a1d40dccf79e625c430c93a8d544c488bbeefc
jafarova1 <gultajvj@code.edu.az>
2023-02-08 08:58:37 -0800

    added file

---
+++ script.js
@@ -0,0 +1,145 @@
+"use strict"
+
+// #Task1
+
+// let n=Number(prompt("n ededini daxil edin:"))
+
+// function check(n){
+//     if(n%3==0 && n%7==0){
+//         alert("eded 3e ve 7e bolunur")
+//     }
+//     else{
+//         alert("eded 3e ve 7e bolunmur")
+//     }
+
+// }
+// check(n)
+
+
+
+
+// #Task2
+
+// let n = Number(prompt("n ededini daxil edin"))
+
+
+
+
+// function calculateFactorial(n) {
+//     if(n>0){
+      
+//          let res = 1;
+//         while (n != 1) {
+//            res = res * n;
+//            n = n - 1;
+         
+     
+       
+//         }      alert (res);
+
+//     }
+//     else{
+//         alert("menfidir");
+//     }
+
+
+// }
+// calculateFactorial(n);
+
+
+// #Task3
+
+// let array = [2,4,6,11,9]; 
+
+// function squareOfNum(array){
+
+//     let sum=0;
+//     let res;
+//     for(let i=0;i<array.length;i++){
+//         if(array[i]%2==0){
+//             sum+=array[i]
+//            res=sum*sum;
+
+            
+//         } 
+//         console.log(res);
+     
+
+    
+      
+//     }
+// }
+// squareOfNum(array)
+
+
+
+// #Task4
+
+// let email=prompt("Add email");
+// let password=prompt("Add password")
+
+// function checkAccount(email,password)
+// {
+//     if(email=="cavid@code.edu.az" && password=="12345"){
+//         alert("girish olundu")
+//     }
+//     else{
+//         alert("Mail ve yaxud password sehvdir")
+//     }
+
+// }
+// checkAccount(email,password)
+
+
+// #Task5 
+
+// let array=[2,4,3,7,11]
+
+// function sumOfOdd(array){
+//     let sum=0;
+//     for (let i=0;i<array.length;i++){
+//         if(array[i]%2==1){
+//             sum+=array[i]
+
+//         } console.log(sum)
+//     }
+// }
+
+// sumOfOdd(array);
+
+
+
+
+
+// #Task6
+
+
+// let array=[2,4,6,10,12,13,9,7,5]
+
+// function countOfEven(array){
+//     let count=0;
+//     for(let i=0;i<array.length;i++){
+//         if(array[i]%2==0){
+//             count+=1
+          
+            
+//         } console.log(count)
+      
+       
+
+//     }
+// }
+// countOfEven(array)
+
+
+
+
+
+
+
+
+
+
+
+
+
